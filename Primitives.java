6346b5a17ffc9d9882789dd7b1ce77dcb907dace
NOEL-19 <noelrico1885@gmail.com>
2024-10-08 14:19:50 +0000

    Done Concatenation

---
+++ Primitives.java
@@ -0,0 +1,15 @@
+public class Primitives {
+    public static void main(String[] args) {
+
+   // print: H3110 w0r1d 2.0 true
+        int number = 3110;
+        byte zero = 0;
+        byte one = 1;
+        float f= 2.0f;
+        boolean B= true;
+        System.out.println("H"+ number+" w"+ zero+ "r"+ one+ "d " + f +" "+ B);
+
+
+    }
+
+}
