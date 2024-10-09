850bff8e65d834ea27251d913544496063442f8b
NOEL-19 <noelrico1885@gmail.com>
2024-10-09 01:46:50 +0000

    Done task

--- Primitives.java
+++ Primitives.java
@@ -4,10 +4,11 @@ public class Primitives {
    // print: H3110 w0r1d 2.0 true
         int number = 3110;
         byte zero = 0;
+        String output = "w" + zero + "r";
         byte one = 1;
         float f= 2.0f;
         boolean B= true;
-        System.out.println("H"+ number+" w"+ zero+ "r"+ one+ "d " + f +" "+ B);
+        System.out.println("H"+ number+ " " + output + one+ "d " + f +" "+ B);
 
 
     }
